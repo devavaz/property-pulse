334e5531fed7b2e9acac34a7ac9ae8ecd7c35254
devavaz <135300758+devavaz@users.noreply.github.com>
2024-02-22 19:00:18 +0900

    Issue

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,17 @@ import Hero from '@/components/Hero'
 import HomeProperties from '@/components/HomeProperties'
 import InfoBoxes from '@/components/InfoBoxes'
 
+
+
 const Home = () => {
+  
   return (
    <section>
      <Hero  />
      <InfoBoxes />
      <HomeProperties />
    </section>
-  )
+  ) 
 }
 
 export default Home
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,16 +1,32 @@
-import properties from '@/properties.json'
 import PropertyCard from '@/components/PropertyCard';
 
 
-const PropertiesPage = () => {
+async function fetchProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/properties`)
+    
+    if (!res.ok) {
+      throw new Error("failed to fetch properties");
+    }
+    return res.json();
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+
+const PropertiesPage = async () => {
+
+  const properties = await fetchProperties();
+
   return (
-    <section class="px-4 py-6">
-      <div class="container-xl lg:container m-auto px-4 py-6">
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
         
          {properties.length === 0 ? (
           <p>No property found</p>
          ): (
-          <div class="grid grid-cols-1 md:grid-cols-3 gap-6"> 
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6"> 
                    {properties.map((property) => (
                   <PropertyCard key={property._id} property={property} />
          ))}
