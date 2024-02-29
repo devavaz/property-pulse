e3e86e24e900f586e01c138dcefa9aa318139947
devavaz <135300758+devavaz@users.noreply.github.com>
2024-03-01 06:55:59 +0900

    Session provider Wrapper

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,25 @@
-import Footer from '@/components/Footer'
-import '../assets/styles/globals.css'
-import Navbar from '../components/Navbar'
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
+import '@/assets/styles/globals.css';
 
 export const metadata = {
-    title: 'PropertyPulse | Find the Perfect Rental',
-    description: 'Find the perfect rental property for you and your family. PropertyPulse is the best place to find your next home.',
-    keywords: 'rental, find rentals, find properties'
-}
+  title: 'PropertyPulse | Find The Perfect Rental',
+  description: 'Find your dream rental property',
+  keywords: 'rental, find rentals, find properties',
+};
 
-
-const MainLayout = ({children}) => {
+const MainLayout = ({ children }) => {
   return (
-    <html lang='en'>
-       <body>
+    <AuthProvider>
+      <html lang='en'>
+        <body>
           <Navbar />
           <main>{children}</main>
           <Footer />
-       </body>
-    </html>
-   
-  )
-}
-
-export default MainLayout
+        </body>
+      </html>
+    </AuthProvider>
+  );
+};
+export default MainLayout;
